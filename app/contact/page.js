ea9512e40ab2f41085d5d63215e67b246c639e6e
Sharif-Miah <farhansharif141077@gmail.com>
2024-01-10 23:11:44 +0600

    nabvar and next project setup

---
+++ app/contact/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function contact() {
+    console.log('This is contact console')
+  return (
+    <div>
+      <h1>This is Contact Page.</h1>
+    </div>
+  )
+}
---
+++ app/loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="flex justify-center align-middle my-80">
+        <p>Loading.......</p>
+    </div>
+  );
+}
